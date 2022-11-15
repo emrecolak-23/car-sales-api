f592f67d7cb3818666fef3696532ae9037589e98
Emre Çolak <emrecolak@Emre.home>
2022-11-15 23:46:02 +0300

    removing and updating records

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch, Query, Delete } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
-
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 
 @Controller('auth')
@@ -13,6 +13,26 @@ export class UsersController {
     @Post('/signup')
     createUser(@Body() dto: CreateUserDto) {
         this.userService.create(dto.email, dto.password)
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.userService.findOne(parseInt(id))
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.userService.find(email)
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string) {
+        return this.userService.remove(parseInt(id))
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() dto: UpdateUserDto) {
+        return this.userService.update(parseInt(id), dto)
     }
 
 }
