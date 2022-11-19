a9634d7cbf7de9e51f16315cf829aa9f7de1cad2
Emre Çolak <emrecolak@emrecolak.local>
2022-11-19 18:34:36 +0300

    connecting an interceptor to dependency injection

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,9 +5,11 @@ import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
 
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService]
+  providers: [UsersService, AuthService, CurrentUserInterceptor]
 })
 export class UsersModule {}
