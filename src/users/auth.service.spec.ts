65f13086af61e113b6f3d08a9c74cd2561574533
Emre Çolak <emrecolak@Emre.home>
2022-11-19 23:14:35 +0300

    unused email to test signin

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,7 +2,7 @@ import { Test } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
 import { User } from "./user.entity";
 import { UsersService } from "./users.service";
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException,NotFoundException } from '@nestjs/common';
 
 describe( 'AuthService', () => {
 
@@ -35,9 +35,7 @@ describe( 'AuthService', () => {
     })
 
     it('throws an error if user signs up with email that is in use', async () => {
-        fakeUsersService.find = () =>
-     
-          Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+        fakeUsersService.find = () => Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
         await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
           BadRequestException,
         );
@@ -51,4 +49,11 @@ describe( 'AuthService', () => {
         expect(salt).toBeDefined()
         expect(hash).toBeDefined()
     })
+    
+    it('throws if signin is called with an unused email', async () => {
+        await expect(
+          service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
+        ).rejects.toThrow(NotFoundException);
+    });    
+    
 })
