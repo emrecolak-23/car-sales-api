406cb1d91043c4818c56d869c01487cfb6b9a456
Emre Çolak <emrecolak@Emre.home>
2022-11-20 23:07:28 +0300

    refactroing to use intelligent mock

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -43,7 +43,7 @@ describe( 'AuthService', () => {
     })
 
     it('throws an error if user signs up with email that is in use', async () => {
-        fakeUsersService.find = () => Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+        await service.signup('asdf@asdf.com', 'asdf')
         await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
           BadRequestException,
         );
@@ -65,10 +65,7 @@ describe( 'AuthService', () => {
     });    
 
     it('throws if an invalid password is provided', async () => {
-        fakeUsersService.find = () =>
-          Promise.resolve([
-            { email: 'asdf@asdf.com', password: 'laskdjf' } as User,
-          ]);
+        await service.signup('laskdjf@alskdfj.com', 'aslsaşdksfl')
         await expect(
           service.signin('laskdjf@alskdfj.com', 'passowrd'),
         ).rejects.toThrow(BadRequestException);
@@ -82,5 +79,12 @@ describe( 'AuthService', () => {
 
    
     })
+
+    it('throws an error if user signs up with email that is in use', async () => {
+        await service.signup('asdf@asdf.com', 'asdf');
+        await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+          BadRequestException,
+        );
+    });
 
 })
