e70ea494772a5f5cb76fe6b15b1c020519f3715b
Emre Çolak <emrecolak@Emre.home>
2022-11-24 23:17:36 +0300

    adding properties to report entity

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -30,4 +30,22 @@ describe('Authentication System', () => {
         expect(email).toEqual(email)
      })
   });
+
+  it('sign up as a new user and then get the currently logged in user', async () => {
+     const email = 'test2@gmail.com'
+     const res = await request(app.getHttpServer())
+                    .post('/auth/signup')
+                    .send({email, password: 'pass123'})
+                    .expect(201)
+    
+     const cookie = res.get('Set-Cookie')
+     
+     const { body } = await request(app.getHttpServer())
+                            .get('/auth/whoami')
+                            .set('Cookie', cookie)
+                            .expect(200)
+
+     expect(body.email).toEqual(email)
+    
+  })
 });
