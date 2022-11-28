62d97d7fa3d5775d65086ba75dbaddaaac3ea425
Emre Çolak <emrecolak@Emre.home>
2022-11-28 20:39:21 +0300

    adding authorization guard

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -15,6 +15,8 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 
+import { AdminGuard } from 'src/guards/admin.guard';
+
 @Controller('reports')
 export class ReportsController {
 
@@ -28,6 +30,7 @@ export class ReportsController {
     }
 
     @Patch('/:id')
+    @UseGuards(AdminGuard)
     approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
         return this.reportsService.changeApproval(id, body.approved)
     }
